1e0080625f92daad3eb29624d11a1f93aab724f7
Juan Ramirez <juanpram2000@gmail.com>
2025-02-03 18:27:45 +0000

    feature: adding color click and drag events

--- client/src/components/drawingBoard/Grid.tsx
+++ client/src/components/drawingBoard/Grid.tsx
@@ -2,24 +2,63 @@ import { useState } from "react";
 
 const ROWS = 32;
 const COLS = 32;
+const BASE_COLOR = "bg-white";
 
-export default function Grid() {
+export default function Grid({
+  selectedColor = "bg-blue-500",
+  debug = true,
+}: {
+  selectedColor?: string;
+  debug?: boolean;
+}) {
   const [grid, setGrid] = useState<string[][]>(
-    Array.from({ length: ROWS }, () => Array(COLS).fill("bg-white"))
+    Array.from({ length: ROWS }, () => Array(COLS).fill(BASE_COLOR))
   );
+  const [hoveredCell, setHoveredCell] = useState<[number, number]>([0, 0]);
+  const [handleGridUpdate, setHandleGridUpdate] = useState<boolean>(false);
+
+  const updateGrid = (cell: [number, number]) => {
+    setHoveredCell(cell);
+    if (handleGridUpdate) {
+      const newGrid = grid.map((row, rowIDX) => {
+        if (rowIDX === hoveredCell[0]) {
+          return row.map((color, colIDX) => {
+            if (colIDX === hoveredCell[1]) {
+              return selectedColor;
+            }
+            return color;
+          });
+        }
+        return row;
+      });
+      setGrid(newGrid);
+    }
+  };
 
   return (
-    <ul className="grid grid-col-32 grid-rows-32 grid-flow-col size-[48rem]">
-      {grid?.map((row, i) => {
-        return row.map((color, j) => {
-          return (
-            <li
-              key={`${i * 10}+${j}`}
-              className={`${color} border-1 border-black hover:bg-slate-400`}
-            ></li>
-          );
-        });
-      })}
-    </ul>
+    <div>
+      {debug && (
+        <span>
+          x: {hoveredCell[0]} y: {hoveredCell[1]}
+        </span>
+      )}
+      <ul
+        className="grid grid-col-32 grid-rows-32 grid-flow-col size-[48rem] cursor-pointer"
+        onMouseDown={() => setHandleGridUpdate(true)}
+        onMouseUp={() => setHandleGridUpdate(false)}
+      >
+        {grid?.map((row, i) => {
+          return row.map((color, j) => {
+            return (
+              <li
+                key={`${i * 10}+${j}`}
+                className={`${color} border-1 border-black hover:bg-slate-400`}
+                onMouseOver={() => updateGrid([i, j])}
+              ></li>
+            );
+          });
+        })}
+      </ul>
+    </div>
   );
 }
